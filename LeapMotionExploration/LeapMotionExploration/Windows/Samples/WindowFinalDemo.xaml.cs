5f333c86e8fee4dd6a8aa8c01104f220aa607910
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-12-30 12:32:58 +0100

    Add a naive cursor.

--- LeapMotionExploration/LeapMotionExploration/Windows/Samples/WindowFinalDemo.xaml.cs
+++ LeapMotionExploration/LeapMotionExploration/Windows/Samples/WindowFinalDemo.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Leap;
+using MyLeap.Listener;
+using MyLeap.Utils;
+using MyLeap.Event;
 
 namespace LeapMotionExploration.Windows.Samples
 {
@@ -19,9 +23,42 @@ namespace LeapMotionExploration.Windows.Samples
     /// </summary>
     public partial class WindowFinalDemo : Window
     {
+
+        private Controller Controller;
+        private LeapListenerOneHandPosition Listener;
+
         public WindowFinalDemo()
         {
             InitializeComponent();
+            Controller = new Controller();
+
+            Listener = new LeapListenerOneHandPosition(LeapUtils.LEFT_MOST_HAND);
+            Controller.AddListener(Listener);
+
+            Listener.OnStateChange += this.OnPositionChange;
+        }
+
+        private void OnPositionChange(LeapEvent leapEvent)
+        {
+            setCursorPosition(leapEvent.Position);
+        }
+
+        private void setCursorPosition(Leap.Vector position)
+        {
+            Application.Current.Dispatcher.Invoke(new Action(() =>
+            {
+                double xPos = cursorContainer.ActualWidth * position.x - leapCursor.Width / 2;
+                double yPos = cursorContainer.ActualHeight * (1 - position.y) - leapCursor.Height / 2;
+                leapCursor.SetValue(Canvas.TopProperty, yPos);
+                leapCursor.SetValue(Canvas.LeftProperty, xPos);
+            }));
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            Controller.RemoveListener(Listener);
+            Controller.Dispose();
+            base.OnClosing(e);
         }
     }
 }
