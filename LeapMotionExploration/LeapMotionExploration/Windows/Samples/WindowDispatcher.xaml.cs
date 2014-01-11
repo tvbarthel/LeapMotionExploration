e50bc57d3a76ff6736702250a5810a9757b11b28
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2014-01-11 15:22:08 +0100

    Implement dispatcher proof concept

--- LeapMotionExploration/LeapMotionExploration/Windows/Samples/WindowDispatcher.xaml.cs
+++ LeapMotionExploration/LeapMotionExploration/Windows/Samples/WindowDispatcher.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MyLeap.Dispatcher;
+using MyLeap.Interface;
+using MyLeap.Event;
+using MyLeap.Utils;
 
 namespace LeapMotionExploration.Windows.Samples
 {
@@ -19,9 +23,30 @@ namespace LeapMotionExploration.Windows.Samples
     /// </summary>
     public partial class WindowDispatcher : Window
     {
+        private CanvasDispatcher _dispatcher;
         public WindowDispatcher()
         {
             InitializeComponent();
+
+            _dispatcher = new CanvasDispatcher(canvas);
+
+            _dispatcher.SetCursor(cursor,LeapUtils.LEFT_MOST_HAND);
+
+            _dispatcher.AddClapListener(left, new ILeapListenerClap( e => 
+            {
+                info.Text  = "Left";           
+            }));
+
+            _dispatcher.AddClapListener(right, new ILeapListenerClap(e =>
+            {
+                info.Text = "Right";
+            }));
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            _dispatcher.OnClose();
+            base.OnClosing(e);
         }
     }
 }
